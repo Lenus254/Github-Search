d039d7a4ca9fbbea33fad31e92f508ffde6a6eb7
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-01-18 00:44:35 +0300

    created user,repo and notfound components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { RepoComponent } from './repo/repo.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    HomepageComponent,
+    NotfoundComponent,
+    RepoComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
