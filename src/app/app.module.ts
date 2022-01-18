b8094cb9d813c6f4284d4e06e5b18b97e2c945da
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-01-18 06:22:37 +0300

    channged background color of homepage and styled profile image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { NotfoundComponent } from './notfound/notfound.component';
 import { RepoComponent } from './repo/repo.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { HomepageComponent } from './homepage/homepage.component';
 import { UserComponent } from './user/user.component';
+import { HttpClientModule } from '@angular/common/http';
+// import { FormSearchComponent } from './form-search/form-search.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    HomepageComponent,
-    NotfoundComponent,
     RepoComponent,
-    UserComponent
+    NotfoundComponent,
+    HomepageComponent,
+    UserComponent,
+    // FormSearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
