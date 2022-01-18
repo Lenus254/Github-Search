f372c77b49ba88564168597a7d80624b240462da
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-01-18 05:33:11 +0300

    imported dependencies into search service

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -1,4 +1,8 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
